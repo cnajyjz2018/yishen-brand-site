8fc574b3eebb2b496da475d71fa86a7f3b195a4f
cnajyjz2018 <52066183+cnajyjz2018@users.noreply.github.com>
2025-12-19 01:20:17 +0800

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,12 +1,9 @@
-<script>
-async function loadHTML(id, url) {
+async function loadComponent(id, url) {
   const el = document.getElementById(id);
   if (!el) return;
   const res = await fetch(url);
   el.innerHTML = await res.text();
 }
-document.addEventListener("DOMContentLoaded", () => {
-  loadHTML("header", "/partials/header.html");
-  loadHTML("footer", "/partials/footer.html");
-});
-</script>
+
+loadComponent("site-navbar", "/components/navbar.html");
+loadComponent("site-footer", "/components/footer.html");
